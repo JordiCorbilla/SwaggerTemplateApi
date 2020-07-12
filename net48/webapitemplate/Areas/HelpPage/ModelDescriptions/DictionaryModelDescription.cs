aac2caaf3b4ec45829b8f53503b97cdd18b1a061
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2020-07-12 12:22:17 +0100

    Swagger UI for .net framework 4.8 template

---
+++ net48/webapitemplate/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace webapitemplate.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ net48/webapitemplate/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace webapitemplate.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
